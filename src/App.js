209c9490d7bc09efd18702794e61d49a1c641501
Andrey Arias <andreyarias35@gmail.com>
2020-01-04 22:55:21 -0800

    add wisesquare component/css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import WiseSquare from './WiseSquare';
+import AnnoyingForm from './AnnoyingForm';
 
 function App() {
   return (
     <div className="App">
+      <h1>React Events Mini Demo</h1>
+      <h4>On mouse leave</h4>
       <WiseSquare />
+      <h4>On key up</h4>
+      <AnnoyingForm />
     </div>
   );
 }
