588783026af55ad655cc1edbb1111c97ea0de6af
Andrey Arias <andreyarias35@gmail.com>
2020-01-04 22:56:29 -0800

    add annoyingform component

---
+++ src/AnnoyingForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class AnnoyingForm extends Component {
+    handleKeyUp(evt) {
+        if (evt.keyCode === 56) {
+          alert("MOAR * PLZ");
+        } else {
+          alert("NO!");
+        }
+      }
+      render() {
+        return (
+          <div>
+            <h3>Try Typing In Here:</h3>
+            <textarea onKeyUp={this.handleKeyUp} />
+          </div>
+        );
+      }
+}
